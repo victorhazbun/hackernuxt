46f489e1743a96182206cb641b71e302722a11ce
victor hazbun <victorhazbun87@gmail.com>
2018-04-05 15:22:01 -0500

    Display Only Available Data in Vue.js and Nuxt

--- components/Items.vue
+++ components/Items.vue
@@ -1,39 +1,43 @@
 <template>
-  <div class="code">
-    <ul class="list pa2">
-      <li class="item" v-for="item in items" :key="item.id">{{item.title}}
-        <div class="score">
-          {{item.score}}
-        </div>
-        <div class="items">
-          {{item.title}}
-        </div>
-        <div class="details">
-          by {{item.by}} {{item.time | timeSince}}
-        </div>
-        <div class="comments">
-          {{item.descendants}} comments
-        </div>
-      </li>
-    </ul>
-  </div>
+    <div class="code">
+        <ul class="list pa2">
+            <li class="item f6" v-for="item in items" :key="item.id">
+                <div class="score f4">
+                    {{item.score}}
+                </div>
+                <div class="title">
+                    {{item.title}}
+                    <template v-if="item.url">
+                        <a class="f7" :href="item.url">{{item.url | hostname}}</a>
+                    </template>
+                </div>
+                <div class="details">
+                    by {{item.by}}
+                    <p class="ma0 i f7">{{item.time | timeSince}} ago</p>
+                </div>
+                <template v-if="item.descendants">
+                    <div class="comments">
+                        {{item.descendants}} comments
+                    </div>
+                </template>
+            </li>
+        </ul>
+    </div>
 </template>
-
 <script>
-  import {mapState} from "vuex"
-
-  export default {
+import { mapState } from "vuex"
+export default {
     computed: mapState([
-      "items"
+        "items"
     ])
-  }
+}
 </script>
-
 <style scoped>
 .item {
     display: grid;
     grid: repeat(4, 1.5em) / repeat(10, 1fr);
     grid-row-gap: 1em;
+    grid-column-gap: 1em;
 }
 .score {
     grid-row: 1 / -1;
@@ -49,10 +53,12 @@
 .comments {
     grid-row: 3 / -1;
     grid-column: 2 / 6;
+    align-self: center;
 }
 .details {
     grid-row: 3 / -1;
     grid-column: 6 / -2;
     justify-self: end;
+    align-self: center;
 }
 </style>
